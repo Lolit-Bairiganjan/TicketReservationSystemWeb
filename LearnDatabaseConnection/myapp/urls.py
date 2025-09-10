3a38de158d8baa79359bb4a9abe4759b2553ac79
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2025-09-10 17:37:57 +0530

    Clean up repository: Keep only DemoProject, remove other learning projects
    - Removed Html Static Files directory
    - Removed LearnDatabaseConnection project
    - Removed myproject directory
    - Repository now contains only the main DemoProject with beautiful UI

--- LearnDatabaseConnection/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.main_page, name='main_page'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-]
